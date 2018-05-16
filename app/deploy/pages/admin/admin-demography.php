c5d128643af7725bd67cfa6be9e2d3d5d8a5471d
seladanghijau <nadzmiidzham@gmail.com>
2018-05-16 08:40:45 +0800

    settle most of chapter 4 & starting chapter 5

--- app/deploy/pages/admin/admin-demography.php
+++ app/deploy/pages/admin/admin-demography.php
@@ -26,15 +26,48 @@ $decodedParliamentData = json_decode($parliamentFile, true);
                             <form action="" method="POST">
                                 <?php for($x=0 ; $x<sizeof($decodedData) ; $x++) { ?>
                                     <h2>
-                                        <b><a data-toggle="collapse" data-target=".collapse-<?= $x ?>" aria-expanded="false" aria-controls="<?= "parliament" . $x ?>"><?= $decodedData[$x]['state_name'] ?></a></b>
+                                        <b><?= $decodedData[$x]['state_name'] ?></b>
                                     </h2>
-                                    <?php for($y=0 ; $y<sizeof($decodedData[$x]['demography']) ; $y++) { ?>
+                                    <div class="row">
+                                        <div class="col-md-offset-1 col-md-4">
+                                            <b>State Demographics</b><br />
+                                            <b><u>Gender:</u></b>
+                                            <div class="form-group">
+                                                <u>Male Totals</u>
+                                                <input class="form-control"
+                                                    type="number"
+                                                    name="<?= $x ?>-state-gender-male"
+                                                    value="<?= $decodedData[$x]['demography']['state']['gender']['male'] ?>" />
+                                            </div>
+                                            <div class="form-group">
+                                                <u>Female Totals</u>
+                                                <input class="form-control"
+                                                    type="number"
+                                                    name="<?= $x ?>-state-gender-female"
+                                                    value="<?= $decodedData[$x]['demography']['state']['gender']['female'] ?>" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-offset-4 col-md-4">
+                                            <a class="btn btn-primary"
+                                                data-toggle="collapse"
+                                                data-target=".collapse-<?= $x ?>"
+                                                aria-expanded="false"
+                                                aria-controls="<?= "parliament" . $x ?>">
+                                                Parliaments
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <?php for($y=0 ; $y<sizeof($decodedData[$x]['demography']['parliament']) ; $y++) { ?>
                                         <div class="row collapse collapse-<?= $x ?>">
                                             <div class="col-md-offset-1 col-md-10">
                                                 <h3>
-                                                    <b>[<?= $decodedData[$x]['demography'][$y]['parliament_code'] ?>] </b>
+                                                    <b>[<?= $decodedData[$x]['demography']['parliament'][$y]['parliament_code'] ?>] </b>
                                                     <?= $decodedParliamentData[$x]['parliament'][$y]['parliament_name'] ?>
                                                 </h3>
+                                                <b>Parliament Demographics</b><br />
+                                                <b><u>Ethnics / Races:</u></b>
                                                 <div class="row">
                                                     <div class="col-md-6">
                                                         <u>Malay Percentage (%)</u>
@@ -42,9 +75,9 @@ $decodedParliamentData = json_decode($parliamentFile, true);
                                                             <input class="form-control"
                                                                 type="number"
                                                                 step="0.01"
-                                                                name="<?= $decodedData[$x]['demography'][$y]['parliament_code'] ?>-malay"
+                                                                name="<?= $decodedData[$x]['demography']['parliament'][$y]['parliament_code'] ?>-malay"
                                                                 placeholder="Malay Percentage (%)"
-                                                                value="<?= $decodedData[$x]['demography'][$y]['race']['malay'] ?>" />
+                                                                value="<?= $decodedData[$x]['demography']['parliament'][$y]['race']['malay'] ?>" />
                                                         </div>
                                                     </div>
                                                     <div class="col-md-6">
@@ -53,9 +86,9 @@ $decodedParliamentData = json_decode($parliamentFile, true);
                                                             <input class="form-control"
                                                                 type="number"
                                                                 step="0.01"
-                                                                name="<?= $decodedData[$x]['demography'][$y]['parliament_code'] ?>-chinese"
+                                                                name="<?= $decodedData[$x]['demography']['parliament'][$y]['parliament_code'] ?>-chinese"
                                                                 placeholder="Chinese Percentage (%)"
-                                                                value="<?= $decodedData[$x]['demography'][$y]['race']['chinese'] ?>" />
+                                                                value="<?= $decodedData[$x]['demography']['parliament'][$y]['race']['chinese'] ?>" />
                                                         </div>
                                                     </div>
                                                 </div>
@@ -66,9 +99,9 @@ $decodedParliamentData = json_decode($parliamentFile, true);
                                                             <input class="form-control"
                                                                 type="number"
                                                                 step="0.01"
-                                                                name="<?= $decodedData[$x]['demography'][$y]['parliament_code'] ?>-indian"
+                                                                name="<?= $decodedData[$x]['demography']['parliament'][$y]['parliament_code'] ?>-indian"
                                                                 placeholder="Indian Percentage (%)"
-                                                                value="<?= $decodedData[$x]['demography'][$y]['race']['indian'] ?>" />
+                                                                value="<?= $decodedData[$x]['demography']['parliament'][$y]['race']['indian'] ?>" />
                                                         </div>
                                                     </div>
                                                     <div class="col-md-6">
@@ -77,9 +110,9 @@ $decodedParliamentData = json_decode($parliamentFile, true);
                                                             <input class="form-control"
                                                                 type="number"
                                                                 step="0.01"
-                                                                name="<?= $decodedData[$x]['demography'][$y]['parliament_code'] ?>-sabah"
+                                                                name="<?= $decodedData[$x]['demography']['parliament'][$y]['parliament_code'] ?>-sabah"
                                                                 placeholder="Sabah Percentage (%)"
-                                                                value="<?= $decodedData[$x]['demography'][$y]['race']['sabah'] ?>" />
+                                                                value="<?= $decodedData[$x]['demography']['parliament'][$y]['race']['sabah'] ?>" />
                                                         </div>
                                                     </div>
                                                 </div>
@@ -90,9 +123,9 @@ $decodedParliamentData = json_decode($parliamentFile, true);
                                                             <input class="form-control"
                                                                 type="number"
                                                                 step="0.01"
-                                                                name="<?= $decodedData[$x]['demography'][$y]['parliament_code'] ?>-sarawak"
+                                                                name="<?= $decodedData[$x]['demography']['parliament'][$y]['parliament_code'] ?>-sarawak"
                                                                 placeholder="Sarawak Percentage (%)"
-                                                                value="<?= $decodedData[$x]['demography'][$y]['race']['sarawak'] ?>" />
+                                                                value="<?= $decodedData[$x]['demography']['parliament'][$y]['race']['sarawak'] ?>" />
                                                         </div>
                                                     </div>
                                                     <div class="col-md-6">
@@ -101,9 +134,9 @@ $decodedParliamentData = json_decode($parliamentFile, true);
                                                             <input class="form-control"
                                                                 type="number"
                                                                 step="0.01"
-                                                                name="<?= $decodedData[$x]['demography'][$y]['parliament_code'] ?>-others"
+                                                                name="<?= $decodedData[$x]['demography']['parliament'][$y]['parliament_code'] ?>-others"
                                                                 placeholder="Others Percentage (%)"
-                                                                value="<?= $decodedData[$x]['demography'][$y]['race']['others'] ?>" />
+                                                                value="<?= $decodedData[$x]['demography']['parliament'][$y]['race']['others'] ?>" />
                                                         </div>
                                                     </div>
                                                 </div>
@@ -139,13 +172,16 @@ if(isset($_POST['submit'])) {
     $decodedData = json_decode($jsonFile, true);
     
     for($x=0 ; $x<sizeof($decodedData) ; $x++) {
+        $decodedData[$x]['demography']['state']['gender']['male'] = $_POST[$x . '-state-gender-male'];
+        $decodedData[$x]['demography']['state']['gender']['female'] = $_POST[$x . '-state-gender-female'];
+        
         for($y=0 ; $y<sizeof($decodedData[$x]['demography']) ; $y++) {
-            $decodedData[$x]['demography'][$y]['race']['malay'] = floatval($_POST[$decodedData[$x]['demography'][$y]['parliament_code'] . '-malay']);
-            $decodedData[$x]['demography'][$y]['race']['chinese'] = floatval($_POST[$decodedData[$x]['demography'][$y]['parliament_code'] . '-chinese']);
-            $decodedData[$x]['demography'][$y]['race']['indian'] = floatval($_POST[$decodedData[$x]['demography'][$y]['parliament_code'] . '-indian']);
-            $decodedData[$x]['demography'][$y]['race']['sabah'] = floatval($_POST[$decodedData[$x]['demography'][$y]['parliament_code'] . '-sabah']);
-            $decodedData[$x]['demography'][$y]['race']['sarawak'] = floatval($_POST[$decodedData[$x]['demography'][$y]['parliament_code'] . '-sarawak']);
-            $decodedData[$x]['demography'][$y]['race']['others'] = floatval($_POST[$decodedData[$x]['demography'][$y]['parliament_code'] . '-others']);
+            $decodedData[$x]['demography']['parliament'][$y]['race']['malay'] = floatval($_POST[$decodedData[$x]['demography']['parliament'][$y]['parliament_code'] . '-malay']);
+            $decodedData[$x]['demography']['parliament'][$y]['race']['chinese'] = floatval($_POST[$decodedData[$x]['demography']['parliament'][$y]['parliament_code'] . '-chinese']);
+            $decodedData[$x]['demography']['parliament'][$y]['race']['indian'] = floatval($_POST[$decodedData[$x]['demography']['parliament'][$y]['parliament_code'] . '-indian']);
+            $decodedData[$x]['demography']['parliament'][$y]['race']['sabah'] = floatval($_POST[$decodedData[$x]['demography']['parliament'][$y]['parliament_code'] . '-sabah']);
+            $decodedData[$x]['demography']['parliament'][$y]['race']['sarawak'] = floatval($_POST[$decodedData[$x]['demography']['parliament'][$y]['parliament_code'] . '-sarawak']);
+            $decodedData[$x]['demography']['parliament'][$y]['race']['others'] = floatval($_POST[$decodedData[$x]['demography']['parliament'][$y]['parliament_code'] . '-others']);
         }
     }
     
